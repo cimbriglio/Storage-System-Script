ec67a25ed20736050bf0369e257a156de521ad0c
cimbriglio <cimbriglio@proton.me>
2026-02-25 11:42:50 -0500

    Added automatically removing black line from csv

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ import curses
 def main_menu(stdscr):
     curses.curs_set(0)
     stdscr.clear()
-
     choices = ["Add Item", "List Item", "Search Item","Exit"]
     current_row = 0
     
     while True:
+
+        with open('inventory.csv', 'r+') as file:
+            inventory_content = file.read()
+            file.seek(0)
+            file.write("\n".join(line for line in inventory_content.splitlines() if line.strip()))
         stdscr.clear()
 
         for idx, choice in enumerate(choices):
@@ -30,12 +34,15 @@ def main_menu(stdscr):
         elif key == curses.KEY_ENTER or key in [10, 13]:
             if choices[current_row] == "Add Item":
                 stdscr.clear()
+                time.sleep(0.25)
                 commands.add_item(stdscr)
             elif choices[current_row] == "List Item":
                 stdscr.clear()
+                time.sleep(0.25)
                 commands.list_item(stdscr)
             elif choices[current_row] == "Search Item":
                 stdscr.clear()
+                time.sleep(0.25)
                 commands.search_item(stdscr)
             elif choices[current_row] == "Exit":
                 break
